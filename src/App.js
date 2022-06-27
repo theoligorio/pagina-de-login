1a7fbfa528a71cb0fc8bbf9bf745d53b7f7be33c
theoligorio <theoligorio04@gmail.com>
2022-06-26 23:45:12 -0300

    pagina de login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputRightElement
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
 
-function App() {
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
+
+const App = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="#464D6A"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg="#E2E8F0" />
+        <Heading color="#E2E8F0">Bem Vindo</Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={4}
+              padding="40px"
+              backgroundColor="#353B51"
+              boxShadow="md"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="#DEE2E6" />}
+                  />
+                  <Input type="email" placeholder="Insira seu E-mail" />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="#CED4DA"
+                    children={<CFaLock color="#CED4DA" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Insira sua Senha"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="40px" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Esconder" : "Mostrar"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link>Esqueceu a Senha?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="gray"
+                width="full"
+              >
+                Entrar
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Box color="#E2E8F0">
+        Não possui uma conta?{" "}
+        <Link color="#E2E8F0" href="#">
+          Cadastre-se
+        </Link>
+      </Box>
+    </Flex>
   );
-}
+};
 
 export default App;
